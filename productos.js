39856b613e55a91d7ea10d00a25f48c68caff1b8
Cristian Leiva <cleiva01@gmail.com>
2024-09-04 14:37:49 -0400

    Inicio de trabajo entrega 3, deficiendo productos

---
+++ productos.js
@@ -0,0 +1,38 @@
+class produto = [  
+
+    {
+        id: 1,
+        nommbre: "leche",
+        precio: 1000,
+    },
+
+    {
+        id: 2,
+        nommbre: "Pan de Molde",
+        precio: 2000,
+    },
+     {
+        id: 3,
+        nommbre: "Queso",
+        precio: 1200,
+        },
+
+        {
+            id: 4,
+            nommbre: "Mermelada",
+            precio: 890,
+        },
+
+        {
+            id: 5,
+            nommbre: "Azucar",
+            precio: 1300,
+            }
+
+
+]
+    
+ 
+   
+    
+   
